f443949cee0135eef5e691aef11dea52c1ecf87d
rbisht03 <riyabisht979@gmail.com>
2024-11-29 11:54:38 +0530

    Added :Roll die to get a number

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.lang.Math;
+import java.util.Random;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Player p1=new Player();
+        Random r=new Random();
+        int roll=r.nextInt(6)+1;
+        System.out.println("value in die:"+roll);
 
     }
 }
